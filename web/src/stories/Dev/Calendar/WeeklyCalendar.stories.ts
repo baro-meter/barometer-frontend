c6aeecd6e4bbc226cafa0ff1354f8822f602c896
lmjing <lmjing4905@gmail.com>
2024-05-12 15:55:09 +0900

    fix storybook error

--- web/src/stories/Dev/Calendar/WeeklyCalendar.stories.ts
+++ web/src/stories/Dev/Calendar/WeeklyCalendar.stories.ts
@@ -1,17 +1,17 @@
-import type {Meta, StoryObj} from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import WeeklyCalendar from '@/components/Calendar/WeeklyCalendar';
+import WeeklyCalendar from "@/components/Calendar/WeeklyCalendar";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Dev/Calendar/WeeklyCalendar',
+  title: "Dev/Calendar/WeeklyCalendar",
   component: WeeklyCalendar,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
+    layout: "centered",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {},
 } satisfies Meta<typeof WeeklyCalendar>;
@@ -21,5 +21,9 @@ type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
-  args: {},
+  args: {
+    year: 2024,
+    month: 1,
+    date: 1,
+  },
 };
