fee00c21316bb86f18fa48787cd5cf922d30f17c
yeorim <yrimxoxo@gmail.com>
2024-05-19 16:51:52 +0900

    [Markup] Progress

---
+++ web/src/markup/components/ProgressView.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import classNames from "classnames/bind";
+import scss from "styles/components/progress.module.scss";
+
+const cn = classNames.bind(scss);
+
+interface ProgressProps {
+  task: string;
+  count: string;
+  width: number;
+  isActive?: boolean;
+}
+
+const ProgressView = ({task, width, count, isActive}: ProgressProps) => {
+  return (
+    <div className={cn("progress", {"is-active": isActive})}>
+      <div className={cn("progress-bar")} role="progressbar" aria-valuenow={width} aria-valuemin={0} aria-valuemax={100} style={{ width: `${width}%` }}></div>
+      <div className={cn("progress-info")}>
+        <strong className={cn("progress-title")}>{task}</strong>
+        <span className={cn("progress-count")}>이번 주 {count}</span>
+      </div>
+    </div>
+  )
+}
+
+export default ProgressView;
