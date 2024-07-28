7fe59f91d612900cddddb11395dd5cda02a8f1bf
lmjing <lmjing4905@gmail.com>
2024-07-28 14:20:48 +0900

    test

--- web/src/stories/Dev/Calendar/DayHeader.stories.ts
+++ web/src/stories/Dev/Calendar/DayHeader.stories.ts
@@ -1,17 +1,17 @@
-import type {Meta, StoryObj} from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import DayHeader from '@/components/Calendar/DayHeader';
+import DayHeader from "@/components/Calendar/DayHeader";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Dev/Calendar/DayHeader',
+  title: "Dev/Calendar/DayHeader",
   component: DayHeader,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
+    layout: "centered",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {},
 } satisfies Meta<typeof DayHeader>;
