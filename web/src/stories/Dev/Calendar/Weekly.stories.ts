35ac8ae1d24fd9df1571cf534f6aa7a5b8509a3a
lmjing <lmjing4905@gmail.com>
2024-08-03 16:10:19 +0900

    add weekly component

---
+++ web/src/stories/Dev/Calendar/Weekly.stories.ts
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Weekly from "@/components/calendar/Weekly";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Dev/Calendar/Weekly",
+  component: Weekly,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {},
+} satisfies Meta<typeof Weekly>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {
+    weekIdx: 3,
+    weekDates: [14, 15, 16, 17, 18, 19, 20],
+    activeDate: 15,
+  },
+};
