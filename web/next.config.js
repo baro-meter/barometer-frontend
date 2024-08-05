56a177e39990fdafb9193d16afef4d7c63349916
lmjing <lmjing4905@gmail.com>
2024-08-05 19:03:00 +0900

    test

--- web/next.config.js
+++ web/next.config.js
@@ -3,26 +3,29 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: process.env.STORYBOOK
+    ? "barometer-frontend/storybook_static"
+    : "",
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
     prependData: `@import "@/styles/abstracts/_variables.scss"; @import "@/styles/abstracts/_mixins.scss";`,
   },
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname, "src");
-    if (process.env.STORYBOOK) {
-      config.module.rules.push({
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[path][name].[ext]",
-              publicPath: (url) => `barometer-frontend/storybook_static/${url}`,
-            },
-          },
-        ],
-      });
-    }
+    // if (process.env.STORYBOOK) {
+    //   config.module.rules.push({
+    //     test: /\.(png|jpe?g|gif|svg)$/i,
+    //     use: [
+    //       {
+    //         loader: "file-loader",
+    //         options: {
+    //           name: "[path][name].[ext]",
+    //           publicPath: (url) => `barometer-frontend/storybook_static/${url}`,
+    //         },
+    //       },
+    //     ],
+    //   });
+    // }
     return config;
   },
 };
