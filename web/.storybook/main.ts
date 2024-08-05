3833ad265daabe9c7fc4fb7eb064aedd24b0062b
lmjing <lmjing4905@gmail.com>
2024-08-05 19:14:21 +0900

    Test

--- web/.storybook/main.ts
+++ web/.storybook/main.ts
@@ -27,6 +27,18 @@ const config: StorybookConfig = {
     // if (configType === "PRODUCTION") {
     //   config.output.publicPath = "/barometer-frontend/storybook_static";
     // }
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|svg)$/i,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[path][name].[ext]",
+            publicPath: (url) => `/barometer-frontend/storybook_static/${url}`,
+          },
+        },
+      ],
+    });
     return config;
   },
 };
--- web/next.config.js
+++ web/next.config.js
@@ -3,19 +3,15 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.STORYBOOK
-    ? "barometer-frontend/storybook_static"
-    : "",
+  // assetPrefix: process.env.STORYBOOK
+  //   ? "barometer-frontend/storybook_static"
+  //   : "",
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
     prependData: `@import "@/styles/abstracts/_variables.scss"; @import "@/styles/abstracts/_mixins.scss";`,
   },
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname, "src");
-    console.log(process.env.STORYBOOK);
-    if (process.env.STORYBOOK) {
-      console.log("Storybook build detected");
-    }
     // if (process.env.STORYBOOK) {
     //   config.module.rules.push({
     //     test: /\.(png|jpe?g|gif|svg)$/i,
