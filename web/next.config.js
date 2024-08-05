9c8560d6f37d2dae6a3fdc6cf2666e936282b145
lmjing <lmjing4905@gmail.com>
2024-08-05 18:09:44 +0900

    Update next config

--- web/next.config.js
+++ web/next.config.js
@@ -3,15 +3,27 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.STORYBOOK
-    ? "/barometer-frontend/storybook_static/"
-    : undefined,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
     prependData: `@import "@/styles/abstracts/_variables.scss"; @import "@/styles/abstracts/_mixins.scss";`,
   },
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    if (process.env.STORYBOOK) {
+      config.module.rules.push({
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name].[ext]",
+              publicPath: (url) =>
+                `/barometer-frontend/storybook_static/${url}`,
+            },
+          },
+        ],
+      });
+    }
     return config;
   },
 };
