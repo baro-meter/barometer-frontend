286a0f9ac9ea75220f823b24e7c7a8dd3dc91a73
lmjing <lmjing4905@gmail.com>
2025-01-12 17:05:26 +0900

    categoryTab 삭제, 동일 기능 마크업 categoryLabel에 적용

--- web/src/markup/components/CategoryLabel.tsx
+++ web/src/markup/components/CategoryLabel.tsx
@@ -1,11 +1,9 @@
-import React from "react";
+import React, { createContext, useEffect, useState } from "react";
 import classNames from "classnames/bind";
 import scss from "@/styles/components/category.module.scss";
 import Image from "next/image";
 import { basePath } from "next.config";
-import { CategoryTabContext } from "@/components/tab/CategoryTab";
-import { useCategory } from "@/hooks/useCategory";
-import { GoalCategoryType } from "@/types/goal";
+import { GoalCategoryType, GoalTypeId } from "@/types/goal";
 
 const cn = classNames.bind(scss);
 
@@ -14,43 +12,59 @@ interface CategoryLabelProps {
 }
 
 export const CategoryLabel = ({ items }: CategoryLabelProps) => {
-  //  categories 정의 프론트 단에서 const로 들고 있어서 고정값으로 들고옴
-  // const { getAllCategoryLableItems } = useCategory();
+  const [activeTabTypeId, setActiveTabTypeId] = useState<
+    GoalTypeId | undefined
+  >();
+
   return (
-    <div className={cn("category-list")}>
-      <CategoryTabContext.Consumer>
-        {({ activeTabTypeId, setActiveTabTypeId }) => {
-          return items.map((category, index) => {
-            const handleChange = () => setActiveTabTypeId(category.typeId);
-            const id = `category${index}`;
-            return (
-              <span key={id} className={cn("category")}>
-                <input
-                  type="radio"
-                  id={id}
-                  name="categoryGroup"
-                  checked={activeTabTypeId === category.typeId}
-                  onChange={handleChange}
-                  className={cn("category-input")}
-                />
-                <label htmlFor={id} className={cn("category-label")}>
-                  {category.typeId !== undefined && (
-                    <Image
-                      src={`${basePath}/img/icon-category.svg`}
-                      width={12}
-                      height={13}
-                      alt=""
-                    />
-                  )}
-                  {category.text}
-                </label>
-              </span>
-            );
-          });
-        }}
-      </CategoryTabContext.Consumer>
-    </div>
+    <CategoryLabelContext.Provider
+      value={{ activeTabTypeId, setActiveTabTypeId }}
+    >
+      <div className={cn("category-list")}>
+        <CategoryLabelContext.Consumer>
+          {({ activeTabTypeId, setActiveTabTypeId }) => {
+            return items.map((category, index) => {
+              const handleChange = () => setActiveTabTypeId(category.typeId);
+              const id = `category${index}`;
+              return (
+                <span key={id} className={cn("category")}>
+                  <input
+                    type="radio"
+                    id={id}
+                    name="categoryGroup"
+                    checked={activeTabTypeId === category.typeId}
+                    onChange={handleChange}
+                    className={cn("category-input")}
+                  />
+                  <label htmlFor={id} className={cn("category-label")}>
+                    {category.typeId !== undefined && (
+                      <Image
+                        src={`${basePath}/img/icon-category.svg`}
+                        width={12}
+                        height={13}
+                        alt=""
+                      />
+                    )}
+                    {category.text}
+                  </label>
+                </span>
+              );
+            });
+          }}
+        </CategoryLabelContext.Consumer>
+      </div>
+    </CategoryLabelContext.Provider>
   );
 };
 
 export default CategoryLabel;
+
+interface CategoryLabelContextType {
+  activeTabTypeId: GoalTypeId | undefined;
+  setActiveTabTypeId: (typeId: GoalTypeId | undefined) => void;
+}
+
+export const CategoryLabelContext = createContext<CategoryLabelContextType>({
+  activeTabTypeId: undefined,
+  setActiveTabTypeId: () => {},
+});
