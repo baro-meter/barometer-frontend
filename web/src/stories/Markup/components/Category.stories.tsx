9e92dab9b79ebe85f1bf2221b5fc325c776d2626
lmjing <lmjing4905@gmail.com>
2025-01-27 14:14:19 +0900

    CategoryLabel storybook error 조치

--- web/src/stories/Markup/components/Category.stories.tsx
+++ web/src/stories/Markup/components/Category.stories.tsx
@@ -10,7 +10,29 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof CategoryLabel>;
 
+const testData = [
+  {
+    text: "전체",
+    order: 0,
+  },
+  {
+    typeId: 1,
+    text: "규칙적인 생활",
+    order: 1,
+  },
+  {
+    typeId: 2,
+    text: "마음의 여유",
+    order: 2,
+  },
+  {
+    typeId: 3,
+    text: "체중 관리",
+    order: 3,
+  },
+];
+
 export const Default: Story = {
-  render: (args) => <CategoryLabel />,
+  render: (args) => <CategoryLabel items={testData} />,
   args: {},
 };
